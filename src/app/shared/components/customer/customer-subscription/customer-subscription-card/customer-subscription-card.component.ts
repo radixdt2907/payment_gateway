4e7b81f20f42ba8a744b18af24b81c1851b2ca8a
radixdt2907 <@1FjOj2bEgifop>
2024-06-17 18:56:53 +0530

    feat: added the customer-subscriptions-card

--- src/app/shared/components/customer/customer-subscription/customer-subscription-card/customer-subscription-card.component.ts
+++ src/app/shared/components/customer/customer-subscription/customer-subscription-card/customer-subscription-card.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import axios from 'axios';
+import { ActivatedRoute, ActivatedRouteSnapshot, Route, Router, RouterLink } from '@angular/router';
+import { SubscriptionsService } from 'src/app/shared/services/subscriptions/subscriptions.service';
 
 @Component({
   selector: 'app-customer-subscription-card',
   templateUrl: './customer-subscription-card.component.html',
-  styleUrls: ['./customer-subscription-card.component.css']
+  styleUrls: ['./customer-subscription-card.component.css'],
 })
 export class CustomerSubscriptionCardComponent implements OnInit {
+  constructor(private router: Router, private route: ActivatedRoute, private subscriptionsService: SubscriptionsService) {}
+
   ngOnInit(): void {
-    axios.get('test')
+    this.redirectToHomeBasedOnCustomerId();
+  }
+
+  private redirectToHomeBasedOnCustomerId = () => {
+    const {customer_id} = this.route.snapshot.queryParams;
+
+    if(!customer_id) this.router.navigate(['']);
+    else this.fetchAllSubscriptions(customer_id)
   }
 
 
+  public fetchAllSubscriptions = (customer_id: string) => {
+
+    this.subscriptionsService.fetchAllSubscriptions(customer_id).subscribe(res => {
+      console.log(res);
+    })
+  }
 }
