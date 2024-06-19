6ddbeccf536e6707c985f4cb40d9d3a4e1a2bef5
radixdt2907 <@1FjOj2bEgifop>
2024-06-19 10:53:23 +0530

    fix: change structure of paymentHistory Model

--- src/app/shared/components/customer/customer-payment-history/customer-payment-history-card/customer-payment-history-card.component.ts
+++ src/app/shared/components/customer/customer-payment-history/customer-payment-history-card/customer-payment-history-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-customer-payment-history-card',
@@ -9,5 +9,5 @@ export class CustomerPaymentHistoryCardComponent {
   
   @Input() paymentHistoryObj: any;
 
-  readonly panelOpenState = signal(false);
+  // readonly panelOpenState = signal(false);
 }
